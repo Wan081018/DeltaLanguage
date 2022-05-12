005e1168e332f4096e972ae29e65880e1aa5df25
Wan081018 <102288109+Wan081018@users.noreply.github.com>
2022-05-13 06:23:07 +0800

    Add files via upload

---
+++ delc/lib/precompiler/precompile.c
@@ -0,0 +1,28 @@
+//==================================================//
+//
+// * Copyright (c) 2022, Wan Buwen
+// * All rights reserved.
+//
+//--------------------------------------------------//
+//
+// * Filename: precompile.c
+// * Author: Wan Buwen
+// * Version: 1.0
+// * Date: 12/05/2022
+//
+//--------------------------------------------------//
+//
+// * to precompile the source file
+// * delc/include/preprocess.h
+//
+//==================================================//
+
+#include "../../include/precompile.h"
+
+int precompile(FILE *fin, FILE *fout){
+	if(ignore_comment(fin, fopen("__TMP_ic.delt", "w"))) return 1;
+	if(include_file(fopen("__TMP_ic.delt", "r"), fopen("__TMP_if.delt", "w"))) return 1;
+	if(change_data(fopen("__TMP_if.delt", "r"), fout)) return 1;
+	return 0;
+}
+
