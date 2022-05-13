c463e23c0823959fd6977d743624f01799d6e9bd
Wan081018 <102288109+Wan081018@users.noreply.github.com>
2022-05-13 17:32:16 +0800

    Add files via upload

---
+++ delc/lib/error/put_error.c
@@ -0,0 +1,34 @@
+//==================================================//
+//
+// * Copyright (c) 2022, Wan Buwen
+// * All rights reserved.
+//
+//--------------------------------------------------//
+//
+// * Filename: put_error.c
+// * Author: Wan Buwen
+// * Version: 1.0
+// * Date: 12/06/2022
+//
+//--------------------------------------------------//
+//
+// * definition of function that handle the errors
+//
+//==================================================//
+
+#include <stdarg.h>
+
+int put_error(int errnum, ...){
+	switch(errnum){
+		case 1:{
+			
+			break;
+		}
+		case 2:{
+			
+			break;
+		}
+	}
+	return 0;
+}
+
