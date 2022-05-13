ef8cd9da1e71741a7f41db2408dd33ba6571b774
Wan081018 <102288109+Wan081018@users.noreply.github.com>
2022-05-13 17:19:10 +0800

    Add files via upload

---
+++ delc/include/error.h
@@ -0,0 +1,29 @@
+//==================================================//
+//
+// * Copyright (c) 2022, Wan Buwen
+// * All rights reserved.
+//
+//--------------------------------------------------//
+//
+// * Filename: error.h
+// * Author: Wan Buwen
+// * Version: 1.0
+// * Date: 12/06/2022
+//
+//--------------------------------------------------//
+//
+// * declaration of functions that handle the errors
+//
+//==================================================//
+
+#ifndef ERROR_H
+#define ERROR_H
+
+// errnum means the type of the error
+//==================================================//
+//
+//==================================================//
+extern int put_error(int errnum, ...);
+
+#endif
+
