fb4f5435a35ea892ef5b0c7c991d4e624455ad79
Wan081018 <102288109+Wan081018@users.noreply.github.com>
2022-05-14 19:14:10 +0800

    Add files via upload

---
+++ delc/lib/precompiler/change_text.c
@@ -0,0 +1,26 @@
+//==================================================//
+//
+// * Copyright (c) 2022, Wan Buwen
+// * All rights reserved.
+//
+//--------------------------------------------------//
+//
+// * Filename: change_text.c
+// * Author: Wan Buwen
+// * Version: 1.0
+// * Date: 14/05/2022
+//
+//--------------------------------------------------//
+//
+// * to change the specified text in the souce file 
+//   into specified form
+// * delc/include/preprocess.h
+//
+//==================================================//
+
+#include <stdio.h>
+
+int change_text(FILE *fin, FILE *fout){
+	return 0;
+}
+
---
+++ delc/lib/precompiler/include_file.c
@@ -0,0 +1,27 @@
+//==================================================//
+//
+// * Copyright (c) 2022, Wan Buwen
+// * All rights reserved.
+//
+//--------------------------------------------------//
+//
+// * Filename: include_file.c
+// * Author: Wan Buwen
+// * Version: 1.0
+// * Date: 14/05/2022
+//
+//--------------------------------------------------//
+//
+// * to include the files into the souce file and do
+//   some other directions
+// * delc/include/preprocess.h
+//
+//==================================================//
+
+#include <stdio.h>
+
+int include_file(FILE *fin, FILE *fout){
+	
+	return 0;
+}
+
