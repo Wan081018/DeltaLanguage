85b8ba1b05295ab4283396334bfb5a21c3da21d5
Wan081018 <102288109+Wan081018@users.noreply.github.com>
2022-05-14 19:16:05 +0800

    Update put_error.c

--- delc/lib/error/put_error.c
+++ delc/lib/error/put_error.c
@@ -8,27 +8,27 @@
 // * Filename: put_error.c
 // * Author: Wan Buwen
 // * Version: 1.0
-// * Date: 12/06/2022
+// * Date: 14/05/2022
 //
 //--------------------------------------------------//
 //
-// * definition of function that handle the errors
+// * to handle the errors during the compilation
+// * delc/include/error.h
 //
 //==================================================//
 
+#include <stdio.h>
 #include <stdarg.h>
 
 int put_error(int errnum, ...){
 	switch(errnum){
 		case 1:{
-			
+			fprintf(stderr, "[ERROR] Unterminated comment.\n");
 			break;
-		}
-		case 2:{
+		} case 2:{
 			
 			break;
 		}
 	}
 	return 0;
 }
-
